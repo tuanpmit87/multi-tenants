b737fe563d69f64e9e9f47b8baad4447e1fb2665
TuanPM[LUV] <phammanhtuan@luvina.net>
2024-01-05 14:06:55 +0700

    commit

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,5 @@
-CREATE TABLE `tenant1`.`users` (
+CREATE DATABASE IF NOT EXISTS `tenant1`;
+CREATE TABLE IF NOT EXISTS `tenant1`.`users` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
 	`user_name` VARCHAR(255) NOT NULL,
 	`full_name` VARCHAR(255) NOT NULL,
@@ -6,7 +7,8 @@ CREATE TABLE `tenant1`.`users` (
   PRIMARY KEY (`user_id`)
 );
 
-CREATE TABLE `tenant2`.`users` (
+CREATE DATABASE IF NOT EXISTS `tenant2`;
+CREATE TABLE IF NOT EXISTS `tenant2`.`users` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
 	`user_name` VARCHAR(255) NOT NULL,
 	`full_name` VARCHAR(255) NOT NULL,
